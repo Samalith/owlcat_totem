b5b8afbf4bbe0db8cc61580c2d543a87f68d8fd0
samael <samael@localhost.localdomain>
2022-04-27 18:47:07 -0400

    Setps 2,3 and scss1.32

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin as vue } from 'vite-plugin-vue2';
+
+const path = require('path');
+
+export default defineConfig({
+  //https://vitejs.dev/config/
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '~bootstrap': 'bootstrap',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/assets/vendors/bootstrap-vue/index.scss";`
+      },
+    },
+  },
+})
